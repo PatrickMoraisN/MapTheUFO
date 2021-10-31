ff000539d0d7b5c83342dfff707a3401b30495a8
Patrick Morais <ppternunes@gmail.com>
2021-10-30 23:28:24 -0300

    install leaflet / add Map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import Main from '../components/Main';
+import Map from '../components/Map';
 
 export default function Home() {
-  return <Main />;
+  return (
+    <>
+      <Main />
+      <Map />
+    </>
+  );
 }
