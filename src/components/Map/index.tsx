85f8e5c5e0ea0c72edb5606f851c2d3a46e5ec33
Patrick Morais <ppternunes@gmail.com>
2021-10-31 12:47:06 -0300

    add Dynamic Import to Map component / config MapContainer

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,11 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={3}
+      style={{ height: '50%', width: '50%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import dynamic from 'next/dynamic';
 import Main from '../components/Main';
-import Map from '../components/Map';
+// import Map from '../components/Map';
+
+const Map = dynamic(() => import('../components/Map'), { ssr: false });
 
 export default function Home() {
   return (
