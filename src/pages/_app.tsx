7782ddc8a2cd3ca2396c2edae5a83958619b24be
Patrick Morais <ppternunes@gmail.com>
2021-11-01 16:31:05 -0300

    MapTheUFO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate</title>
+        <title>MapTheUFO</title>
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
